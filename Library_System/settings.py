18d9d926ec871ca4922cae28764ce8f0da523a66
thomasjames433 <thomaspjames433@gmail.com>
2024-12-07 09:45:54 +0530

    .

--- Library_System/settings.py
+++ Library_System/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ym#3v9=jgj$w76+o*s87pn*tmn-@b-nk88++zrp&36h%v6!hr='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
