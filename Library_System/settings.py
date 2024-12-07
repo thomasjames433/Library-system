9e38d723fbff001ccad59e2cdae119990ebcd7d0
thomasjames433 <thomaspjames433@gmail.com>
2024-12-07 09:47:42 +0530

    .

--- Library_System/settings.py
+++ Library_System/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ym#3v9=jgj$w76+o*s87pn*tmn-@b-nk88++zrp&36h%v6!hr=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
